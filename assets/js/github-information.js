4a7989bd05aa3e44bb86169b9d3560d96eb0eb31
Janisroz <janisrozkalejs@gmail.com>
2023-10-14 17:26:17 +0000

    Fix bugs for the gh-user-data & gh-repo-data

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -16,6 +16,9 @@ function userInformation(user) {
 }
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("")
+    $("#gh-repo-data").html("")
+
     var username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html('<h2>Please eneter a GitHub username</h2>');
@@ -70,3 +73,5 @@ function fetchGitHubInformation(event) {
         }
     );
 }
+
+$(document).ready(fetchGitHubInformation);
